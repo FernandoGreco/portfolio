c7d88670372306eb51126a058ce75aa93070a9dd
Fernando Greco <greco-fernando@hotmail.com>
2020-12-09 17:58:04 -0300

    inicio ok

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,28 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.less']
 })
+
 export class InicioComponent implements OnInit {
-
-  div = document.getElementById('log');
-  textos = ['Desenvolvimento de Sistemas', 'Layout Responsivo', 'Front-End e Back-End [Júnior]','PHP, Mysql, JavaScript e CSS','Bootstrap, SASS, Gulp, Angular 2+, Composer, Laravel, GIT, NPM'];
  
+   @ViewChild('log') myDiv: ElementRef;
 
-  
 
   constructor() { 
-    
-  }
+      }
 
   ngOnInit() {
-    this.rodape('ala');
-  }
- 
 
-   escrever(str, done) {
+   // this.myDiv.nativeElement.innerHTML = "teste";
+    
+   let div = this.myDiv.nativeElement;
+
+  let textos = ['Desenvolvedor Front-End Júnior', 'Type Script, PHP, Mysql, JavaScript e CSS'];
+ 
+  rodape(textos,div);
+
+  function rodape(conteudos,div){
+    var atual = -1;
+    var cont = 0;
+  function prox(cb){
+    cont++;
+    if (atual < conteudos.length - 1) atual++;
+    else atual = 0;
+    var str = conteudos[atual];
+   
+    escrever(str,div, function(){
+      limpar(prox,div);
+    });
+  }
+  prox(prox);
+  }
+
+  function escrever(str, div, done) {
     var char = str.split('').reverse();
     var typer = setInterval(function() {
         if (!char.length) {
@@ -30,23 +48,27 @@ export class InicioComponent implements OnInit {
             return setTimeout(done, 2000); // só para esperar um bocadinho
         }
         var next = char.pop();
-        this.div.innerHTML += next;
+        div.innerHTML += next;
     }, 80);
 }
 
- limpar(done) {
-    var char = this.div.innerHTML;
-    var nr = char.length;
-    var typer = setInterval(function() {
-        if (nr-- == 0) {
-            clearInterval(typer);
-            return done();
-        }
-        this.div.innerHTML = char.slice(0, nr)
-    }, 25);
+function limpar(done,div) {
+  var char = div.innerHTML;
+  var nr = char.length;
+  var typer = setInterval(function() {
+      if (nr-- == 0) {
+          clearInterval(typer);
+          return done();
+      }
+      div.innerHTML = char.slice(0, nr)
+  }, 25);
 }
 
- rodape(conteudos) {
+ 
+  /*
+
+
+  rodape(conteudos) {
     var atual = -1;
     var cont = 0;
     function prox(cb){
@@ -62,10 +84,49 @@ export class InicioComponent implements OnInit {
   prox(prox);
 
 }
+  
 
-teste(){
-  console.log('test ok');
-
-}
-
-}
+  function escrever(str, done) {
+     var char = str.split('').reverse();
+     var typer = setInterval(function() {
+         if (!char.length) {
+             clearInterval(typer);
+             return setTimeout(done, 2000); // só para esperar um bocadinho
+         }
+         var next = char.pop();
+         this.div.innerHTML += next;
+     }, 80);
+ }
+ 
+  limpar(done) {
+     var char = this.div.innerHTML;
+     var nr = char.length;
+     var typer = setInterval(function() {
+         if (nr-- == 0) {
+             clearInterval(typer);
+             return done();
+         }
+         this.div.innerHTML = char.slice(0, nr)
+     }, 25);
+ }
+ 
+  rodape(conteudos) {
+     var atual = -1;
+     var cont = 0;
+     function prox(cb){
+     cont++;
+     if (atual < conteudos.length - 1) atual++;
+     else atual = 0;
+     var str = conteudos[atual];
+    
+      this.escrever(str, function(){
+       this.limpar(this.prox);
+     });
+   }
+   prox(prox);
+ 
+ }
+ */
+  }
+  
+ }
--- src/app/sobre/sobre.component.ts
+++ src/app/sobre/sobre.component.ts
@@ -1,20 +1,152 @@
-import { Component, OnInit,ViewChild,ElementRef } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-sobre',
   templateUrl: './sobre.component.html',
   styleUrls: ['./sobre.component.less']
 })
-export class SobreComponent implements OnInit {
 
-  canvas: ElementRef<HTMLCanvasElement>;
+export class SobreComponent implements OnInit  {
 
-  private ctx: CanvasRenderingContext2D;
+  @ViewChild('myCanvas') myDiv: ElementRef;
+   public context: CanvasRenderingContext2D;
+ 
 
-
-  constructor() { }
+  constructor() {    
+    
+      }
 
   ngOnInit() {
+           
+      //edição do canvas  
+      this.context = this.myDiv.nativeElement.getContext('2d');
+      // console.log(this.context);
+
+      //edição do css
+      this.myDiv.nativeElement.style.position= 'fixed';
+      this.myDiv.nativeElement.style.zIndex = '-1';
+     console.log( this.myDiv.nativeElement.style);
+     //  let ElementDiv = this.myDiv.nativeElement;
+     //  console.log(ElementDiv.width);
+   
+
+   let particles = [];
+   let particleSize = 2;
+   let maxParticles = 40;
+   let threshold = 70;
+
+
+    for (let i = 0; i < maxParticles; i++) {
+      let particle = {
+        x: Math.random() * this.myDiv.nativeElement.width,
+        y: Math.random() * this.myDiv.nativeElement.height,
+        vx: Math.random(),
+        vy: Math.random()
+      }
+      particles.push(particle);
+    }
+
+ 
+
+      animate(this.myDiv.nativeElement, this.context,particles,maxParticles);
+
+      function animate(ElementDiv,context,particles,maxParticles) {    
+           
+        context.clearRect(0, 0, ElementDiv.width, ElementDiv.height);
+        for (let i = 0; i < maxParticles; i++) {
+          let particle = particles[i];
+         context.fillRect(particle.x - particleSize / 2, particle.y - particleSize / 2, particleSize, particleSize);
+          for (let j = 0; j < maxParticles; j++) {
+            if (i != j) {
+              let particle2 = particles[j];
+              let distanceX = Math.abs(particle.x - particle2.x);
+              let distanceY = Math.abs(particle.y - particle2.y);
+              if (distanceX < threshold && distanceY < threshold) {
+                context.lineWidth = ((threshold * 2) - (distanceX + distanceY)) / 50;
+                let color = 200 - Math.floor(distanceX + distanceY);
+               context.strokeStyle = 'rgb(' + color + ',' + color + ',' + color + ')';
+               line(particle, particle2,context);
+              }
+            }
+          }
+          particle.x = particle.x + particle.vx;
+          particle.y = particle.y + particle.vy;
+          if (particle.x > ElementDiv.width - particleSize || particle.x < particleSize)
+            particle.vx = -particle.vx;
+          if (particle.y > ElementDiv.height - particleSize || particle.y < particleSize)
+            particle.vy = -particle.vy;
+        }
+        window.requestAnimationFrame(this.animate);
+        
+      }
+
+
+      
+     function line(particle, particle2,context) {
+      context.beginPath();
+      context.moveTo(particle.x, particle.y);
+      context.lineTo(particle2.x, particle2.y);
+      context.stroke();
+    }
+       
+  
+        /*
+    //this.myDiv.nativeElement.style.backgroundColor='red';
+        
+
+     function line(particle, particle2) {
+      this.context.beginPath();
+      this.context.moveTo(this.particle.x, this.particle.y);
+      this.context.lineTo(this.particle2.x, this.particle2.y);
+      this.context.stroke();
+    }
+     
+    function animate() {
+      this.context.clearRect(0, 0, this.myDiv.nativeElement.width, this.myDiv.nativeElement.height);
+      for (let i = 0; i < this.maxParticles; i++) {
+        let particle = this.particles[i];
+        this.context.fillRect(particle.x - particleSize / 2, particle.y - particleSize / 2, particleSize, particleSize);
+        for (let j = 0; j < maxParticles; j++) {
+          if (i != j) {
+            let particle2 = particles[j];
+            let distanceX = Math.abs(particle.x - particle2.x);
+            let distanceY = Math.abs(particle.y - particle2.y);
+            if (distanceX < threshold && distanceY < threshold) {
+              this.context.lineWidth = ((threshold * 2) - (distanceX + distanceY)) / 50;
+              let color = 200 - Math.floor(distanceX + distanceY);
+              this.context.strokeStyle = 'rgb(' + color + ',' + color + ',' + color + ')';
+              line(this.particle, this.particle2);
+            }
+          }
+        }
+        particle.x = particle.x + particle.vx;
+        particle.y = particle.y + particle.vy;
+        if (particle.x > this.myDiv.nativeElement.width - particleSize || particle.x < particleSize)
+          particle.vx = -particle.vx;
+        if (particle.y > this.myDiv.nativeElement.height - particleSize || particle.y < particleSize)
+          particle.vy = -particle.vy;
+      }
+      window.requestAnimationFrame(animate);
+    }
+     
+    let particles = [];
+    let particleSize = 2;
+    let maxParticles = 40;
+    let threshold = 70;
+    for (let i = 0; i < maxParticles; i++) {
+      let particle = {
+        x: Math.random() * this.myDiv.nativeElement.width,
+        y: Math.random() * this.myDiv.nativeElement.height,
+        vx: Math.random(),
+        vy: Math.random()
+      }
+      particles.push(particle);
+    }
+    
+    animate();
+*/
+
+
 
   }
 
