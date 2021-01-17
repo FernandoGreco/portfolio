7b278ba5f6ed4bb10b3c51cb1097c30e99d9f496
Fernando Greco <greco-fernando@hotmail.com>
2021-01-16 22:38:51 -0300

    sobre ok

--- src/app/contato/contato.component.ts
+++ src/app/contato/contato.component.ts
@@ -23,13 +23,14 @@ copyTextFromElement(elementID) {
   let element = document.getElementById(elementID); //select the element
   let elementText = element.textContent; //get the text content from the element
   this.copyText(elementText); //use the copyText function below
+  navigator.clipboard.writeText(elementText);
 }
 
 //If you only want to put some Text in the Clipboard just use this function
 // and pass the string to copied as the argument.
 copyText(text) {
  //console.log(text);
-navigator.clipboard.writeText(text);
+//navigator.clipboard.writeText(text);
  //navigator.clipboard.readText(text);
 // text.select();
 //document.execCommand('copy');
