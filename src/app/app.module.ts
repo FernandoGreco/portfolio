ee2d7bd006dd6f45f083160869472b9eee0f9904
Fernando Greco <greco-fernando@hotmail.com>
2021-01-21 16:28:07 -0300

    não deu certo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InicioComponent } from './inicio/inicio.component';
@@ -34,7 +34,8 @@ import { GraficoOndasComponent } from './grafico-ondas/grafico-ondas.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/grafico-ondas/grafico-ondas.component.ts
+++ src/app/grafico-ondas/grafico-ondas.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import {Chart} from 'node_modules/chart.js';
 
 @Component({
   selector: 'app-grafico-ondas',
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GraficoOndasComponent implements OnInit {
 
+  @ViewChild('myChart')
+  myCanvas: ElementRef<HTMLCanvasElement>;
+
+  public ctx: CanvasRenderingContext2D;
+
   constructor() { }
 
+
+   loadCity(local){
+
+    document.getElementById("cidade").innerHTML = "Previsão Surf " + local;
+    console.log(local);
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+    //    cidade(this);
+      }
+     
+    }; 
+    //http://servicos.cptec.inpe.br/XML/cidade/851/todos/tempos/ondas.xml   
+   //pega o codigo da cidade escolhida
+    xhttp.open("GET", "http://servicos.cptec.inpe.br/XML/listaCidades?city="+local, true);
+    xhttp.send();
+  }
+
+
+ 
   ngOnInit() {
+
+   
+   
+    
+  
   }
 
 }
